8d479ca29dbebe1ad0b188eda5cd4f1d77ef8622
0x2DF <p.ares@hotmail.com>
2018-02-27 13:43:36 -0800

    queries
    falta logica en addUser para verifica que no existan ciertos datos bajo otro usuario (esta incluido la consulta) y query que devuelve el id de job (tambien incluido).

--- AdministradorEmpleados/connection.js
+++ AdministradorEmpleados/connection.js
@@ -73,10 +73,64 @@ exports.getPaymentHistory = function (idUser) {
     })
 }
 
+exports.addPayment = function (idUser, date, amount) {
+    return new Promise(function (resolve, reject) {
+        var query = 'insert into Payment (idUser, date, amount) values (\'' + idUser + '\', ' + date + ', 0, ' + amount + ');';
+        connection.query(query,function (err, rows) {
+            if(err){
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
 exports.getPersonalInformation = function (idUser) {
     return new Promise(function (resolve, reject) {
         var query = 'select p.*, j.jobName from Person as p inner join Job as j on p.job where p.id = '+ idUser +';';
         connection.query(query,function (err,rows) {
+            if(err){
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+exports.addVaction = function (idUser, date, numberDays) {
+    return new Promise(function (resolve, reject) {
+        var query = 'insert into vacation (date, idUser, state, numberDays) values (\'' + date + '\', ' + idUser + ', 0, ' + numberDays + ');';
+        connection.query(query,function (err, rows) {
+            if(err){
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+exports.getUnapprovedVactions = function () {
+    return new Promise(function (resolve, reject) {
+        var query = 'select p.firstName, p.secondName, p.firstLastName, p.secondLastName ,v.* from vacation v inner join person p on v.idUser = p.id where state = 0;';
+        connection.query(query,function (err, rows) {
+            if(err){
+                reject(err);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+exports.addUser = function (accountNumber, firstName, secondName, firstLastName, secondLastName, address, cellphone, housephone, email, job, birthDate, startAtCompany, username, password, identification) {
+    return new Promise(function (resolve, reject) {
+		
+		// query para ver si ya existe un usuario con ese username, email, accountNumber, identification
+		var query = 'select id from person where userName LIKE \'' + username + '\' OR email LIKE \'' + email + '\' OR accountNumber LIKE \'' + accountNumber + '\' OR identification LIKE \'' + identification + '\';'
+        // query que devuelve una unica fila con el id del job
+		var query = 'select idJob from job where jobName LIKE \'' + job + '\';';
+		//var jobId = ...
+		var query = 'insert into person (accountNumber, firstName, secondName, firstLastName, secondLastName, address, cellphone, housephone, email, job, birthDate, startAtCompany, userName, password, administrator, identification) values (' + accountNumber + ', \'' + firstName + '\', \'' + secondName + '\', \'' + firstLastName + '\', \'' + secondLastName + '\', \'' + address + '\', ' + cellphone + ', ' + housephone + ', \'' + email + '\', ' + jobId + ', \'' + birthDate + '\', \'' + startAtCompany + '\', \'' + username + '\', \'' + bcrypt.hashSync(password,10) '\', 0, ' + identification + ');';
+        connection.query(query,function (err, rows) {
             if(err){
                 reject(err);
             }
