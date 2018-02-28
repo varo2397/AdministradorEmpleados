18d8e457f01bb1e11e0aad478c8e388dd78bed15
Wikkan <josuji-alfa@hotmail.com>
2018-02-28 00:17:07 -0600

    Consultas

--- AdministradorEmpleados/SP/searchIdentification.sql
+++ AdministradorEmpleados/SP/searchIdentification.sql
@@ -1,27 +1,19 @@
 CREATE PROCEDURE `searchIdentification`(
-IN identification VARCHAR(45),
-OUT firstName VARCHAR(45),
-OUT secondName VARCHAR(45),
-OUT firstLastName VARCHAR(45),
-OUT secondLastName VARCHAR(45),
-OUT address VARCHAR(1000),
-OUT cellphone DECIMAL(8,0),
-OUT housephone DECIMAL(8,0),
-OUT email VARCHAR(45),
-OUT jobName VARCHAR(45),
-OUT birthDate DATE,
-OUT identification DECIMAL(9, 0)
+IN identifacation DECIMAL(9,0),
+OUT result INT
 )
 BEGIN
-	SELECT p.firstName INTO firstName FROM person p WHERE p.identification = identification;
-    SELECT p.secondName INTO secondName FROM person p WHERE p.identification = identification;
-    SELECT p.firstLastName INTO firstLastName FROM person p WHERE p.identification = identification;
-    SELECT p.secondLastName INTO secondLastName FROM person p WHERE p.identification = identification;
-    SELECT p.address INTO address FROM person p WHERE p.identification = identification;
-    SELECT p.cellphone INTO cellphone FROM person p WHERE p.identification = identification;
-    SELECT p.housephone INTO housephone FROM person p WHERE p.identification = identification;
-    SELECT p.email INTO email FROM person p WHERE p.identification = identification;
-    SELECT p.jobName INTO jobName FROM person p INNER JOIN job j ON p.job = j.idJob WHERE p.identification = identification;
-    SELECT p.birthName INTO birthName FROM person p WHERE p.identification = identification;
-    SELECT p.identificaction INTO identificaction FROM person p WHERE p.identification = identification;
+    IF EXISTS (SELECT p.identifacation FROM person p WHERE p.identifacation LIKE identifacation)
+	THEN
+		SET result = 1;
+        
+        SELECT p.firstName, p.secondName, p.firstLastName, p.secondLastName, p.address, p.cellphone,
+		   p.housephone, p.email, j.jobName, p.birthDate, p.identification
+		FROM person p 
+		INNER JOIN job j
+			ON j.idJob = p.job
+		WHERE p.identifacation LIKE identifacation;
+    ELSE
+		SET result = 0;
+    END IF
 END
--- AdministradorEmpleados/SP/searchUserName.sql
+++ AdministradorEmpleados/SP/searchUserName.sql
@@ -1,27 +1,19 @@
 CREATE PROCEDURE `searchUserName`(
 IN userName VARCHAR(45),
-OUT firstName VARCHAR(45),
-OUT secondName VARCHAR(45),
-OUT firstLastName VARCHAR(45),
-OUT secondLastName VARCHAR(45),
-OUT address VARCHAR(1000),
-OUT cellphone DECIMAL(8,0),
-OUT housephone DECIMAL(8,0),
-OUT email VARCHAR(45),
-OUT jobName VARCHAR(45),
-OUT birthDate DATE,
-OUT identification DECIMAL(9, 0)
+OUT result INT
 )
 BEGIN
-	SELECT p.firstName INTO firstName FROM person p WHERE p.userName = userName;
-    SELECT p.secondName INTO secondName FROM person p WHERE p.userName = userName;
-    SELECT p.firstLastName INTO firstLastName FROM person p WHERE p.userName = userName;
-    SELECT p.secondLastName INTO secondLastName FROM person p WHERE p.userName = userName;
-    SELECT p.address INTO address FROM person p WHERE p.userName = userName;
-    SELECT p.cellphone INTO cellphone FROM person p WHERE p.userName = userName;
-    SELECT p.housephone INTO housephone FROM person p WHERE p.userName = userName;
-    SELECT p.email INTO email FROM person p WHERE p.userName = userName;
-    SELECT p.jobName INTO jobName FROM person p INNER JOIN job j ON p.job = j.idJob WHERE p.userName = userName;
-    SELECT p.birthName INTO birthName FROM person p WHERE p.userName = userName;
-    SELECT p.identificaction INTO identificaction FROM person p WHERE p.userName = userName;
+    IF EXISTS (SELECT p.userName FROM person p WHERE p.userName LIKE userName)
+	THEN
+		SET result = 1;
+        
+        SELECT p.firstName, p.secondName, p.firstLastName, p.secondLastName, p.address, p.cellphone,
+		   p.housephone, p.email, j.jobName, p.birthDate, p.identification
+		FROM person p 
+		INNER JOIN job j
+			ON j.idJob = p.job
+		WHERE p.userName LIKE userName;
+    ELSE
+		SET result = 0;
+    END IF
 END
