2b4782849b47c11144ac1c2e8ccd21bde376d265
Wikkan <josuji-alfa@hotmail.com>
2018-02-28 00:18:43 -0600

    Identification

--- AdministradorEmpleados/SP/searchIdentification.sql
+++ AdministradorEmpleados/SP/searchIdentification.sql
@@ -1,9 +1,9 @@
 CREATE PROCEDURE `searchIdentification`(
-IN identifacation DECIMAL(9,0),
+IN identification DECIMAL(9,0),
 OUT result INT
 )
 BEGIN
-    IF EXISTS (SELECT p.identifacation FROM person p WHERE p.identifacation LIKE identifacation)
+    IF EXISTS (SELECT p.identifacation FROM person p WHERE p.identifacation LIKE identification)
 	THEN
 		SET result = 1;
         
@@ -12,8 +12,8 @@ BEGIN
 		FROM person p 
 		INNER JOIN job j
 			ON j.idJob = p.job
-		WHERE p.identifacation LIKE identifacation;
-    ELSE
+		WHERE p.identification LIKE identification;
+	ELSE
 		SET result = 0;
     END IF
 END
